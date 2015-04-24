5d8062cdd87421626c0866503904868a52858736
Alex Carroll <alexcarroll.mn@gmail.com>
2015-04-24 14:52:28 -0500

    new

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var port = (process.env.VCAP_APP_PORT || 3000);
+var host = (process.env.VCAP_APP_HOST || 'localhost');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(port, host);
+console.log('Server running at http://'+host+':'+port+'/');
+
