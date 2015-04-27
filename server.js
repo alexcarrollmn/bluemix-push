d22eb99705ce5397f27fa8fcbee5497e24160720
Alex Carroll <alexcarroll.mn@gmail.com>
2015-04-27 11:20:25 -0500

    push proof

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var port = (process.env.VCAP_APP_PORT || 3000);
 var host = (process.env.VCAP_APP_HOST || 'localhost');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('Hello World\n I was pushed from GitHub\n ');
 }).listen(port, host);
 console.log('Server running at http://'+host+':'+port+'/');
 
